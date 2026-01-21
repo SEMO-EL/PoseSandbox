02d661c6d22ee35412caa510be78a1662cd3e1ea
SEMO <92227654+SEMO-EL@users.noreply.github.com>
2026-01-21 20:16:07 +0000

    Update app.js

--- app.js
+++ app.js
@@ -909,18 +909,10 @@ try {
 
   /* ---------------------------- Input routing ---------------------------- */
 
- input.on("pointerdown", (evt) => {
-  selection.onPointerDown(evt.originalEvent);
-
-  // ✅ If you clicked an imported mesh, promote selection to its root
-  const sel = selection.getSelected?.();
-  if (sel?.userData?.isImportedModel && sel.userData.importRoot) {
-    selection.setSelection(sel.userData.importRoot);
-  }
-
-  attachGizmoForCurrentMode();
-});
-
+  input.on("pointerdown", (evt) => {
+    selection.onPointerDown(evt.originalEvent);
+    attachGizmoForCurrentMode();
+  });
 
   input.on("keydown", (evt) => {
     const e = evt.originalEvent;
